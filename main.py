6f98cd7ec2b62d9677580519b0f791cd11e2ab4d
Balamast <brwitunsky@gmail.com>
2026-06-02 12:17:08 -0400

    Add sprite groups for updatable and drawable objects

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from ast import While
-
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
 def main():
-    
+
     print("Starting Asteroids with pygame version 2.6.1 ")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -13,12 +11,16 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     #instantiate player
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+   
 
     while True:
-        dt = clock.tick(60) / 1000.0
-        log_state()
+        
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
@@ -27,16 +29,19 @@ def main():
         screen.fill("black")
 
         #update player
-        player.update(dt)
+        updatable.update(dt)
 
         #draw player
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
 
         
 
         #flip display
         pygame.display.flip()
 
+        dt = clock.tick(60) / 1000.0
+        log_state()
         
         
           
